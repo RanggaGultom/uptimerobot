0a68026e8d89611dc8564774413c1e9bebd50c86
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-07-13 09:32:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const axios = require("axios")
 const urls = ["https://kayoo.glitch.me","https://mate-mat8ika.glitch.me","https://messa-music.glitch.me","https://missas.glitch.me","https://syreg-fe.glitch.me"]
-const uptimerobo = async () => {
-    setInterval(() => {
+setInterval(() => {
             urls.forEach(url => {
             axios.get(url).then(() => console.log("Pong at " + Date.now())).catch(() => {});
         })
     }, 60 * 1000);
-};
-
-uptimerobo();
