6be66e13b95785e281e2974b6bf0c5c4aa8ac753
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-07-19 13:46:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const axios = require("axios")
-const urls = ["https://kayoo.glitch.me","https://mate-mat8ika.glitch.me","https://messa-music.glitch.me","https://messa-iuy.glitch.me","https://syreg-fe.glitch.me",
-              "https://giveaway200.glitch.me","https://giveaway500.glitch.me","https://hydra-fior4e.glitch.me"]
+const urls = ["link project"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
