2d58b362af604f36357fb2dc9bc735a1d97f0e9d
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-07-19 13:58:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://discord-tutoril.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
